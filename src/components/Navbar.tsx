68d7c4bd8a1f115113b0299257ea57b3798cca60
NjugunaBrian <bryunnjuguna300@gmail.com>
2024-02-22 18:17:27 +0300

    feat: added ShoppingCartContext

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
 import { NavLink } from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 const cartIcon = <FontAwesomeIcon icon={faCartShopping} />
 
 
 
 export function Navbar(){
+
+    const {openCart, cartQuantity} = useShoppingCart();
     
     return(
         <div className="p-5 shadow-sm bg-white flex items-center justify-between">
@@ -21,10 +24,13 @@ export function Navbar(){
                     Store
                 </NavLink>
             </nav>
-            <button className="relative px-3 py-2 rounded-full border hover:bg-blue-500">
+            
+            {cartQuantity > 0 && <button onClick={openCart} className="relative px-3 py-2 rounded-full outline-[1px black] hover:bg-blue-500">
                 {cartIcon}
-                <div className="absolute rounded-full bg-red-600 px-2 flex items-center justify-center text-white bottom-0 right-0 translate-x-1/4 translate-y-1/4">3</div>
-            </button>
+                <div className="absolute rounded-full bg-red-600 px-2 flex items-center justify-center text-white bottom-0 right-0 translate-x-1/4 translate-y-1/4">
+                    {cartQuantity}
+                </div>
+            </button>} 
         </div>
     )
 }
--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,4 @@
+import { useShoppingCart } from "../context/ShoppingCartContext";
 import { formatCurrency } from "../utilities/formatCurrencies";
 
 type StoreItemProps = {
@@ -9,41 +10,42 @@ type StoreItemProps = {
 
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+    const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart();
 
-    const quantity = 0;
+    const quantity = getItemQuantity(id);
 
     return (
         <>
             <div className="flex flex-col">
                 <img src={imgUrl} className="h-50 object-cover" />
                 <div>
-                    <div className="flex justify-between items-center p-2">
+                    <div key={id} className="flex justify-between items-center p-2">
                         <span>{name}</span>
                         <span className="text-gray-600">{formatCurrency(price)}</span>
                     </div>
                 </div>
                 <div className="my-auto">
                     {quantity === 0 ?
-                    <button className="bg-blue-500 text-white w-full p-2 rounded-lg"> + Add to Cart
-                    </button> 
-                    :
-                    <div className="flex flex-col items-center justify-center text-white gap-1">
-                        <div className="flex items-center justify-center gap-1">
-                            <button className="bg-blue-500 hover:bg-blue-800 p-2 rounded-lg">
-                                +
-                            </button>
-                            <div className="text-black">
-                            <span className="text-2xl">{quantity}</span> in cart
+                        (<button className="bg-blue-500 hover:bg-blue-800 text-white w-full p-2 rounded-lg" onClick={() => increaseCartQuantity(id)}> Add to Cart
+                        </button>)
+                        :
+                        (<div className="flex flex-col items-center justify-center text-white gap-1">
+                            <div className="flex items-center justify-center gap-1">
+                                <button onClick={() => increaseCartQuantity(id)} className="bg-blue-500 hover:bg-blue-800 p-2 rounded-lg">
+                                    +
+                                </button>
+                                <div className="text-black">
+                                    <span className="text-2xl">{quantity}</span> in cart
+                                </div>
+                                <button onClick={() => decreaseCartQuantity(id)} className="bg-blue-500 hover:bg-blue-800 p-2 rounded-lg">
+                                    -
+                                </button>
                             </div>
-                            <button className="bg-blue-500 hover:bg-blue-800 p-2 rounded-lg">
-                                -
+                            <button onClick={() => removeFromCart(id)} className="bg-red-600 hover:bg-red-800 rounded-lg px-2 py-1">
+                                Remove
                             </button>
-                        </div>
-                        <button className="bg-red-600 hover:bg-red-800 rounded-lg px-2 py-1">
-                            Remove
-                        </button>
 
-                    </div>
+                        </div>)
                     }
                 </div>
             </div>
